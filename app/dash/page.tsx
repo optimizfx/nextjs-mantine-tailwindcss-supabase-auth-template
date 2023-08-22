d14eb2611060fb9c23c006692a1890e88dd87281
OPTIMIZFX <optimizfx@gmail.com>
2023-08-22 22:39:51 +1000

    start mantine 7

---
+++ app/dash/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mantine/core";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      page
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default page;
